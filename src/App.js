f9d073444224522589f4af102f8fefbc4112a340
carlangcana <cgangcana@up.edu.ph>
2024-04-15 22:59:55 +0800

    UI: Improved component sizes

--- src/App.js
+++ src/App.js
@@ -26,6 +26,41 @@ function App() {
     return numbers;
   };
 
+  // Mapping of numbers to custom labels
+  const bingoLabels = {
+    1: "ICS Founder's Son",
+    2: "The Bicolana Computing Mom",
+    3: "Has 8 cats",
+    4: "Has a green pet parrot",
+    5: "Enjoys freediving",
+    6: "#DataBAE",
+    7: "Environmental Advocate and loves chicken",
+    8: "Enjoys baking",
+    9: "Mermaid",
+    10: "Enjoys watching UAAP live",
+    11: "Hindi mapakali pag walang gulay ang pagkain",
+    12: "The K-pop Stan",
+    13: "Okay ka",
+    14: "The history enthusiast",
+    15: "HAHAHAHA",
+    16: "2022 Jowable Award winner",
+    17: "ICS Oppa",
+    18: "Has a blog named Uptime",
+    19: "IGN: bladar",
+    20: "One Grande please",
+    21: "Loves to cook",
+    22: "Rides a bike to campus",
+    23: "Star wars nerd",
+    24: "Mr D.J.",
+    25: "Proud member of Google Dev Group PH",
+    26: "The Parallel Computing Expert",
+    27: "(A'+R')'MGIE",
+    28: "The Songer",
+    29: "Lights, Camera, Action!",
+    30: "Luv Instructor"
+    // Add more labels as needed
+  };
+
   const [bingoNumbers, setBingoNumbers] = useState(generateBingoCard());
 
   // Handle randomize button click
@@ -39,9 +74,9 @@ function App() {
       <div className="bingo-card">
         {bingoNumbers.map((number, index) => (
           <div key={index} className="bingo-cell">
-            {/* Display the number or image based on the Bingo number */}
-            {number}
-        </div>
+            {/* Display the label corresponding to the Bingo number */}
+            {bingoLabels[number]}
+          </div>
         ))}
       </div>
     );
@@ -50,14 +85,13 @@ function App() {
   return (
     <div className="App">
       <div className="App-header">
-        
-        <img className="confettiA" src={confetti}/>
+        <img className="confettiA" src={confetti} alt="Confetti" />
         <ul className='bingo-header'>
-          <li><img src={BImage}/></li>
-          <li><img src={IImage}/></li>
-          <li><img src={NImage}/></li>
-          <li><img src={GImage}/></li>
-          <li><img src={OImage}/></li>
+          <li><img src={BImage} alt="B" /></li>
+          <li><img src={IImage} alt="I" /></li>
+          <li><img src={NImage} alt="N" /></li>
+          <li><img src={GImage} alt="G" /></li>
+          <li><img src={OImage} alt="O" /></li>
         </ul>
         <div className="grid-container">
           {renderBingoCard()}
@@ -67,10 +101,9 @@ function App() {
           <p className="footer">&copy; ICS Socials Committee. 2024</p>
         </div>
         <div>
-          <img className="confettiB" src={confetti}/>
+          <img className="confettiB" src={confetti} alt="Confetti" />
         </div>
       </div>
-      
     </div>
   );
 }
