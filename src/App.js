0be0ee0d36265d3c16d20eb2972d00bf78cb04b8
carlangcana <cgangcana@up.edu.ph>
2024-04-15 20:39:26 +0800

    chore: installed packages for deploying react app on github pages

--- src/App.js
+++ src/App.js
@@ -46,7 +46,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Bingo Card</h1>
+        <div className="grid-container">
         {renderBingoCard()}
+        </div>
         <button className="randomize-button" onClick={handleRandomizeClick}>
           Randomize Numbers
         </button>
